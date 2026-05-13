d1c26e9fd7e14336ec4aa0002649e8a1448023e6
Yuli Rumahorbo <yulirumahorbo.dev@gmail.com>
2026-05-13 16:16:56 +0700

    style: Add Pemasukan & Pengeluaran to MenuList

--- src/screens/Home/components/MenuList.js
+++ src/screens/Home/components/MenuList.js
@@ -1,30 +1,41 @@
 import { useNavigation } from "@react-navigation/native";
 import { MenuIcon } from "../../../components";
+import { ScrollView } from "react-native";
 
 export default function MenuList() {
   const navigation = useNavigation();
   return (
-    <>
+    <ScrollView showsVerticalScrollIndicator={false}>
+      <MenuIcon
+        onPress={() => navigation.navigate("Pemasukan")}
+        iconName="cash-plus"
+        label="PEMASUKAN"
+        color="green"
+      />
+      <MenuIcon
+        onPress={() => navigation.navigate("Pengeluaran")}
+        iconName="cash-minus"
+        label="PENGELUARAN"
+        color="red"
+      />
       <MenuIcon
         onPress={() => navigation.navigate("Kalkulator")}
         iconName="calculator-variant"
         label="KALKULATOR"
         color="orange"
       />
-
       <MenuIcon
         onPress={() => navigation.navigate("Store")}
         iconName="pencil-box-multiple"
         label="INPUT NAMA ANGGOTA"
         color="blue"
       />
-
       <MenuIcon
         onPress={() => navigation.navigate("Pinjaman")}
         iconName="cash"
         label="PINJAMAN BARU"
         color="red"
       />
-    </>
+    </ScrollView>
   );
 }
